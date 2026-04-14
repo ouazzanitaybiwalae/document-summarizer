aa2fb500bd2e6777103866bd976f5d2fa8cab105
walaeOuazzani <ouazzanitaybiwalae@gmail.com>
2026-04-14 22:23:59 +0100

    fix file preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
+nltk.download('punkt')
+nltk.download('stopwords')
+
 def clean_text(text):
     words = word_tokenize(text.lower())
     stop_words = set(stopwords.words('english'))
